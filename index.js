8b879996e9b5263ba71ff95b07a05ef925107ac4
Wenbo.Alex.Fan <wenbo.alex.fan@gmail.com>
2022-04-29 16:53:03 +0800

    use ymssx img repo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { CardNode, renderDeckPDF, getMultiData } = require('ygo-card');
 const fs = require('fs');
 
-const DEFAULT_IMAGE_BASE = 'https://gitee.com/msk86/pics/raw/master/500';
+const DEFAULT_IMAGE_BASE = 'https://gitee.com/ymssx/pics/raw/master/500';
 const OUTPUT_PATH = './output';
 const YDK_PATH = './resources/deck';
 const MOLD_PATH = './node_modules/ygo-card/dist/mold';
